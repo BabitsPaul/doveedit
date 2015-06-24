9dc6b58a67883ee9f1d2a3522ca18c565770d8f2
BabitsPaul <paul.babits@yahoo.de>
2015-06-24 18:12:44 +0200

    Commandline syntaxmodel update

--- util/sequence/src/dove/util/sequence/SequenceUtil.java
+++ util/sequence/src/dove/util/sequence/SequenceUtil.java
@@ -1,7 +1,7 @@
 package dove.util.sequence;
 
 public class SequenceUtil {
-    public static Sequence createSequence(String txt) {
-        return new Sequence(txt);
+    public static SubSequence createSequence(String txt) {
+        return new SubSequence(txt);
     }
 }
